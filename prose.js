ec8595a1e7ed71be87595949ac60f61104be2cf2
gabrielprc <gabrielpablorc@gmail.com>
2017-02-04 20:48:24 -0300

    Configuration

---
+++ prose.js
@@ -0,0 +1,18 @@
+
+/**
+ * Natural language-to-pseudocode compiler.
+ * @exports prose-js
+ */
+var prose = {
+	/**
+	 * Returns the pseudocode for the given text.
+	 *
+	 * @param {string} string - Text to parse.
+	 */
+
+	parseToPseudocode: function(parsable) {
+		return parsable
+	}
+};
+
+module.exports = prose;
