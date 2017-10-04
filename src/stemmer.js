2f11c00ea383052d39cd1622911ab6b71fed712a
gabrielprc <gabrielpablorc@gmail.com>
2017-10-04 20:36:17 -0300

    Improving verb stemmer

--- src/stemmer.js
+++ src/stemmer.js
@@ -23,8 +23,14 @@ function Stemmer() {
 		for (var i = 0; i < strings.length; i++) {
 			if (tags[i] === 'VERB') {
 				var stem = PorterStemmerEs.stem(strings[i]);
+
+				//	Check the stems for relevant verbs and replace for its appropiate expression
 				if (stem === 'ser' || stem === 'se' || stem === 'estar' || stem === 'est' || stem === 'esta') {	//	Replace infinitive of the verb 'to be' with the relevant translation for pseudocode
 					strings[i] = 'es';
+				} else if (stem === 'mostr' || stem === 'muestr') {
+					strings[i] = 'mostrar';
+				} else if (stem === 'evalu') {
+					strings[i] = 'evaluar';
 				}
 			}
 		}
